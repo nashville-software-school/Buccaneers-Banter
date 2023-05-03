54f1f3adb5a54decf0bb7527f1449f314673c37b
Steve <chortlehoort@gmail.com>
2023-05-03 10:29:50 -0500

    Removed obsolete imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,8 @@
 import dynamic from 'next/dynamic';
-import StoryList from '../components/storylist'
-import ShipMates from '../components/shipmates'
-import WantedPoster from '../components/wantedposter'
-import { useEffect, useState } from 'react'
 
 const Home = dynamic(() => import('../components/home'), {
     ssr: false
 });
-
-
 
 export default function Index() {
     return (
